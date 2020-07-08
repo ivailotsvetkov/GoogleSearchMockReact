1582a8910f5a887f057ddb626cfd85ca5c98cb7b
ivaylotsv <ivaylo@pitcherific.com>
2020-07-09 00:50:24 +0200

    Updates

---
+++ precache-manifest.f0c965e0f2d3308090d133fa0cd81a9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f58f1e726f0fdc49d18a74202f8d740",
+    "url": "/GoogleSearchMockReact/index.html"
+  },
+  {
+    "revision": "9371ce6aeba0d43091cc",
+    "url": "/GoogleSearchMockReact/static/css/main.b1625bb6.chunk.css"
+  },
+  {
+    "revision": "25f1a9cbef2b0df5b39b",
+    "url": "/GoogleSearchMockReact/static/js/2.d516b782.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/GoogleSearchMockReact/static/js/2.d516b782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9371ce6aeba0d43091cc",
+    "url": "/GoogleSearchMockReact/static/js/main.d33c54af.chunk.js"
+  },
+  {
+    "revision": "6c47ee380414e8c4bc89",
+    "url": "/GoogleSearchMockReact/static/js/runtime-main.94173b6d.js"
+  },
+  {
+    "revision": "ce621bfcb6f2457fcd5da4411b40093d",
+    "url": "/GoogleSearchMockReact/static/media/logo.ce621bfc.jpg"
+  }
+]);
